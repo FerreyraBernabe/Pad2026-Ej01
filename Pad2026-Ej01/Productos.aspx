c823d4b3df0312580ae415e8aa877164df0b9244
bernbae <bernabeferreyra78@gmail.com>
2026-09-09 15:35:42 -0300

    add: implementacion pagina Productos add: tabla de productos con datos recuperados de la BD

---
+++ Pad2026-Ej01/Productos.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="Productos" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Productos.aspx.cs" Inherits="Pad2026_Ej01.Productos" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <header>
+            <h1>Consultá nuestra lista de productos</h1>
+        </header>
+        <div class="search-bar">
+            <label>Buscar</label>
+            <input type="text"/>
+            <asp:Button ID="btnBuscar" type="submit" runat="server" Text="Realizar búsqueda"/>
+        </div>
+        <asp:GridView ID="gvProductos" runat="server" Height="267px" Width="476px">
+            
+        </asp:GridView>
+    </main>
+
+</asp:Content>
---
+++ Pad2026-Ej01/Productos.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Pad2026_Ej01
+{
+    public partial class Productos : System.Web.UI.Page
+    {
+        
+        private void LoadProducts() 
+        {
+            SqlConnection connection = new SqlConnection(
+                "Data Source=(LocalDB)\\MSSQLLocalDB;" +
+                "AttachDbFilename=\"C:\\Users\\User\\Documents\\U\\4to año\\" +
+                "Programación de Aplicaciones Distribuidas\\Pad2026Ej01\\Pad2026-Ej01\\Pad2026-Ej01\\App_Data\\StarCO.mdf\";" +
+                "Integrated Security=True");
+            connection.Open();
+            SqlCommand query = new SqlCommand("SELECT IdProd," +
+                " Descripcion, " +
+                "Precio " +
+                "FROM Producto", connection);
+            DataTable data = new DataTable();
+            using (SqlDataAdapter adapter = new SqlDataAdapter(query)) 
+            {
+                adapter.Fill(data);
+            }
+            gvProductos.DataSource = data;
+            gvProductos.DataBind();
+            
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            {
+                LoadProducts();
+            }
+        }
+    }
+}
